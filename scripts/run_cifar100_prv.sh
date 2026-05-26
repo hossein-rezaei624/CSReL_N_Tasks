24e677de8cb5187d10777b59aed5fbb848737efd
Hossein Rezaei <hr505@cl.cam.ac.uk>
2026-05-26 23:54:35 +0330

    Update run_cifar100_prv.sh

--- scripts/run_cifar100_prv.sh
+++ scripts/run_cifar100_prv.sh
@@ -1,6 +1,6 @@
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-local_path='./results/split_cifar100/test309'  # set your output path
+local_path='./results/split_cifar100/test001'  # set your output path
 dataset='splitcifar100'
 setting='der'
 data_path=''
